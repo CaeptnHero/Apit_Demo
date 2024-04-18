c4dfd0faa7339daf54a9915d416ed87074dd3af8
CaeptnHero <63815774+CaeptnHero@users.noreply.github.com>
2024-04-18 13:37:11 +0200

    finished
    Json data is prepared

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Model.Model;
 
 @RestController
+
+//Baseroute for Controller
 @RequestMapping("/model")
 public class Controller {
 	
 	//Object who gets the Values
-	@GetMapping("/{modelName}/{modelAdress}")
-	public Model getModelDetails(@PathVariable String modelName, @PathVariable String modelAdress) {
-		return new Model("Max", "Berlin");
-
+		@GetMapping("/modelName")
+		public Model getModelDetails(@PathVariable String modelName, @PathVariable String modelAdress) {
+			return new Model("Max", "Berlin");
+			//http://localhost:8080/model/modelname/Max
+			//Correct URL		
+			
 	}
 }
