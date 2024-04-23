f161d561ec43ffc2bfb7e267f3aadb091d73781f
CaeptnHero <63815774+CaeptnHero@users.noreply.github.com>
2024-04-23 11:39:50 +0200

    Shortened the URL

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -2,7 +2,6 @@ package com.example.demo.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Model.Model;
@@ -15,9 +14,9 @@ import com.example.demo.Model.Model;
 @RequestMapping("/model")
 public class Controller {
 	
-	@GetMapping("/{modelName}/{modelAdress}")
-    public Model getModelDetails(@PathVariable String modelName, @PathVariable String modelAdress) {
-        return new Model(modelName, modelAdress);
+	@GetMapping
+    public Model getModelDetails() {
+        return new Model("Max", "Berlin");
         //http:localhost:8080/model/Max/Berlin
         //Correct URL
         //Output: {"modelName":"Max","modelAdress":"Berlin"}
