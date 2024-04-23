404918d76fb1a4e0f84a5c436076d05c51411c07
CaeptnHero <63815774+CaeptnHero@users.noreply.github.com>
2024-04-23 10:14:21 +0200

    Working Backend
    Output fits

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -1,4 +1,5 @@
 package com.example.demo.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -6,18 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Model.Model;
 
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 
 //Baseroute for Controller
 @RequestMapping("/model")
 public class Controller {
 	
-	//Object who gets the Values
-		@GetMapping("/modelName")
-		public Model getModelDetails(@PathVariable String modelName, @PathVariable String modelAdress) {
-			return new Model("Max", "Berlin");
-			//http://localhost:8080/model/modelname/Max
-			//Correct URL		
-			
+	@GetMapping("/{modelName}/{modelAdress}")
+    public Model getModelDetails(@PathVariable String modelName, @PathVariable String modelAdress) {
+        return new Model(modelName, modelAdress);
+        //http:localhost:8080/model/Max/Berlin
+        //Correct URL
+        //Output: {"modelName":"Max","modelAdress":"Berlin"}
 	}
 }
+
